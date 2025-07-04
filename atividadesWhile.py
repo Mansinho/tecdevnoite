ebe88d8ddd16a10867a80db3faa6f5f5a1aa942e
Mansinho <168103045+Mansinho@users.noreply.github.com>
2025-07-03 21:16:52 -0300

    Create atividadesWhile.py

---
+++ atividadesWhile.py
@@ -0,0 +1,21 @@
+# populacaoA = 80000
+# populacaoB = 200000
+# taxaA = 0.03
+# taxaB = 0.015
+# anos = 0
+
+# while populacaoA < populacaoB:
+#     populacaoA += populacaoA * taxaA
+#     populacaoB += populacaoB * taxaB
+#     anos += 1 
+
+# print(f'Serão necessários {anos} anos para que a população A ultrapasse ou iguale a população B')
+
+numeros = 0
+cont = 0
+
+while numeros > 50:
+
+    numeros += cont + 2
+
+print(numeros)
