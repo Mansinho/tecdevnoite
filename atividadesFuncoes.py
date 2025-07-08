3e3955991ecee0e46bc6d70c23689adf03e7298d
Mansinho <levimansinho56@gmail.com>
2025-07-07 21:08:26 -0300

    atividadeFuncoes.py

---
+++ atividadesFuncoes.py
@@ -0,0 +1,18 @@
+# def calcular_media():
+#     soma = 0
+#     quantidade = int(input("Digite quantas notas deseja somar: "))
+
+#     for i in range(quantidade):
+#         nota = float(input(f"Digite a nota {i + 1}: "))
+#         soma += nota
+
+#     media = soma / quantidade
+#     print(f"A média das notas é: {media:.2f}")
+
+# # Chamando a função
+# calcular_media()
+
+
+def contar_numeros():
+    numero = int(input('Digite um número: '))
+    
