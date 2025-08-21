2b02a6a72eed52da74e80b98a2ce7ea1aaeb3a77
Mansinho <168103045+Mansinho@users.noreply.github.com>
2025-08-21 20:39:58 -0300

    Dia 21/08

--- DEV_DESKTOP/Dia20do8/exercicios_dia20.py
+++ DEV_DESKTOP/Dia20do8/exercicios_dia20.py
@@ -30,30 +30,71 @@
 
 # root.mainloop()
 
+
+
+# EXERCICIO 2
+# import tkinter as tk
+# from tkinter import messagebox
+
+# def resultado_escolar():
+#     try:
+#         nota = float(entry_nota.get())
+#         if nota >= 7 and nota <= 10:
+#             messagebox.showinfo("Resultado", "O aluno foi aprovado!")
+
+#         elif nota >= 5 and nota < 7:
+#             messagebox.showinfo("Resultado", "O aluno está de recuperação!")
+
+#         elif nota < 5 and nota >= 0:
+#             messagebox.showinfo("Resultado", "O aluno foi reprovado!")
+
+#         else:
+#             messagebox.showinfo("Resultado", "Nota inválida.")
+
+#     except ValueError:
+#         messagebox.showerror("Erro", "Por favor, digite apenas números.")
+
+# root = tk.Tk()
+# root.title("Resultado escolar")
+# root.geometry("300x300")
+
+# tk.Label(root, text="Digite a nota do aluno: ").pack(pady=15)
+# entry_nota = tk.Entry(root)
+# entry_nota.pack(pady=10)
+
+# tk.Button(root, text="Confirmar", command=resultado_escolar).pack(pady=10)
+
+# root.mainloop()
+
+
+
+# EXERCICIO 3
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox 
 
-def resultado_escolar():
+def numeros_inteiros():
     try:
-        nota = int(input("Digite a nota do aluno: "))
-        if nota >= 7 and nota <= 10:
-            messagebox.showinfo("Resultado", "O aluno foi aprovado!")
-
-        elif nota >= 5 and nota < 7:
-            messagebox.showinfo("Resultado", "O aluno está de recuperação!")
-
-        elif nota < 5 and nota >= 0:
-            messagebox.showinfo("Resultado", "O aluno foi reprovado!")
+        numero =int(entry_numero.get())
+        if numero < 0:
+            messagebox.showinfo("Resultado", "O número é negativo.")
+        
+        elif numero > 0:
+            messagebox.showinfo("Resultado", "O número é positivo.")
 
         else:
-            messagebox.showinfo("Resultado", "Nota inválida.")
-
+            messagebox.showinfo("Resultado", "Digite um número inteiro!")
+        
     except ValueError:
-        messagebox.showerror("Erro", "Por favor, digite apenas números.")
+        messagebox.showerror("Erro", "Por favor, digite apenas números inteiros!")
 
-    root = tk.Tk()
-    root.title("Resultado escolar")
-    root.geometry("300x300")
+root =  tk.Tk()
+root.title("Verificar números inteiros.")
+root.geometry("300x300")
 
-    tk.Label(root, text=nota).pack(pady=5)
+tk.Label(root, text="Digite um número inteiro: ").pack(pady=15)
+entry_numero = tk.Entry(root)
+entry_numero.pack(pady=10)
 
+tk.Button(root, text="Confirmar", command=numeros_inteiros).pack(pady=10)
+
+root.mainloop()
